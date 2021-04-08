5b3f157d4da97b0dd9bd0565d06b6226917c8af5
Florian Quirin <info@bytemind.de>
2021-04-08 19:46:06 +0200

    v0.9.2; tweaks to processor.js and functions to check 'isInitialized' + 'isProcessing'

--- src/processor.js
+++ src/processor.js
@@ -3,7 +3,7 @@ if (!(typeof SepiaFW == "object")){
 }
 (function (parentModule){
 	var WebAudio = parentModule.webAudio || {};
-	WebAudio.version = "0.9.1";
+	WebAudio.version = "0.9.2";
 	
 	//Preparations
 	var AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -326,17 +326,17 @@ if (!(typeof SepiaFW == "object")){
 						}else if (event.data.moduleResponse){
 							//RESPONSE to "on-demand" request
 							//TODO: ignore?
-						}else if (moduleSetup.sendToModules){
+						}else if (moduleSetup.sendToModules){		//TODO: when do we best clean-up 'sendToModules' to avoid empty loops (processNodes[n] = null)?
 							//data for processing or custom event?
 							if (event.data.moduleEvent){
 								//EVENT
 								moduleSetup.sendToModules.forEach(function(n){
-									if (!processNodes[n].ignoreSendToModules) processNodes[n].sendToModule({ctrl: {action: "handle", data: event.data}});
+									if (processNodes[n] && !processNodes[n].ignoreSendToModules) processNodes[n].sendToModule({ctrl: {action: "handle", data: event.data}});
 								});
 							}else{
 								//PROCESS (default)
 								moduleSetup.sendToModules.forEach(function(n){
-									if (!processNodes[n].ignoreSendToModules) processNodes[n].sendToModule({ctrl: {action: "process", data: event.data}});
+									if (processNodes[n] && !processNodes[n].ignoreSendToModules) processNodes[n].sendToModule({ctrl: {action: "process", data: event.data}});
 								});
 							}
 						}
@@ -689,6 +689,13 @@ if (!(typeof SepiaFW == "object")){
 				if (options.onrelease) options.onrelease();
 				if (callback) callback();
 			});
+		}
+
+		thisProcessor.isInitialized = function(){
+			return isInitialized;
+		}
+		thisProcessor.isProcessing = function(){
+			return isProcessing;
 		}
 	}
 	
